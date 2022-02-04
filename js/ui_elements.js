de8ced32159d6b04439b8a42b3227b49983b5f72
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-04 23:41:45 +0000

    add: file to store refs to all UI elements

---
+++ js/ui_elements.js
@@ -0,0 +1,4 @@
+const ITEMS_IN_BASKET_DISPLAY = document.getElementById("items_basket");
+const CLOSE_CASH_DRAW_POPUP = document.getElementById("close_cash_draw");
+const MAIN_POS_BODY = document.querySelector(".main_pos_body");
+const KEYPAD_INPUT_DISPLAY = document.getElementById("keypad_input_display")
