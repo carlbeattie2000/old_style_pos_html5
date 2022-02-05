751b09052dc261966b245c98b86e7695fc534142
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 01:00:29 +0000

    modify: tmp document.onload to load product data (messy)

--- js/global_functions.js
+++ js/global_functions.js
@@ -7,3 +7,32 @@ function endClickMeasurement() {
     var now = new Date();
     return now-start_time
 }
+
+document.body.onload = () => {
+  getProducts()
+    .then(returnedProducts =>{
+    POS_SYSTEM_DEFAULT.LOAD_ITEMS_LIST(returnedProducts["products"]);
+    })
+    .finally(() => {
+      let elementsToAdd = '';
+
+      POS_SYSTEM_DEFAULT.ITEMS_LIST.forEach(product => {
+        elementsToAdd += `
+          <div class="lookup-item">
+            <img src="${product.imageURL}">
+            <div class="item-details">
+              <p>$${product.price}</p>
+              <p>${product.weight}g</p>
+              <p>${product.tax}%</p>
+              <p>${product.category}</p>
+            </div>
+            <div class="add-item">
+              <button>Add</button>
+            </div>
+          </div>
+        `
+      })
+      
+      FIND_ITEMS_ITEM_DISPLAY.innerHTML = elementsToAdd
+    })
+}
