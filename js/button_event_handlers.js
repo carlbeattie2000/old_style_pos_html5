780a728c9ca684ae6cb466c28eaabeac0bddc5ba
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 00:57:36 +0000

    modify: button events that change the input type

--- js/button_event_handlers.js
+++ js/button_event_handlers.js
@@ -6,6 +6,14 @@ CLOSE_CASH_DRAW_BUTTON.addEventListener("click", () => {
   toggleCloseCashDrawPopup();
 })
 
+FIND_BUTTON.addEventListener("click", () => {
+  toggleFindItemsPopup();
+})
+
+CLOSE_ITEM_LOOKUP.addEventListener("click", () => {
+  toggleFindItemsPopup();
+})
+
 CLEAR_BUTTON.addEventListener("click", () => {
   const newKeyPadValue = KEYPAD_INPUT_DISPLAY.value.slice(0, KEYPAD_INPUT_DISPLAY.value.length-1);
   handleKeyPadClear(newKeyPadValue);
@@ -20,6 +28,46 @@ CLEAR_BUTTON.addEventListener("mouseup", () => {
   if (endClickMeasurement() > 700) {
     handleKeyPadClearFull();
   }
+})
+
+DISCOUNT_PERCENTAGE_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("%");
+})
+
+PRICE_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("$");
+})
+
+PLU_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("PLU");
+})
+
+BARCODE_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("barcode");
+})
+
+WEIGHT_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("g");
+})
+
+COUPON_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("coupon_code");
+})
+
+GIFT_CARD_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("gift_card_code");
+})
+
+CHECK_BUTTON.addEventListener("click", () => {
+  handleKeyPadClearFull();
+  handleKeyPadInputTypeChange("check_number")
 })
 
 
