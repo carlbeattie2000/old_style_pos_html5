5bd412095833a84640b59a082c5973de52d409fe
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 01:52:08 +0000

    modify: changed right side main screen, POS_DEFAULT revamp = more confusion

--- js/pos_default.js
+++ js/pos_default.js
@@ -1,3 +1,6 @@
+// This whole page is a complete mess, and i'm not sure how nearly all of it, is going to actually connect to anything else vise versa.
+
+
 const POS_SYSTEM_VARIABLES = {
   KEYPAD_INPUT_TYPE: "barcode",
   CHANGE_KEYPAD_INPUT_TYPE: function(type) {
@@ -17,5 +20,70 @@ const POS_SYSTEM_VARIABLES = {
   ITEMS_LIST: [],
   LOAD_ITEMS_LIST: function(list) {
     this.ITEMS_LIST = list;
+  }
+}
+
+const POS_ACCOUNTING = {
+  cashInRegisterStartOfDay: 300,
+  cashInRegister: 300,
+  profit: 0,
+  loss: 0,
+  transactions: [],
+  newTransaction: function(details) {
+    // wow, need sleep before approaching this again, was about to forget about this, before somehow breaking through my tired blank mind,
+    // and remembering i will need to calac the amount a customer givens in money and the change we give back.
+    this.cashInRegister += details.price;
+    return this.transactions.push(details)
+  }
+}
+
+const POS_NEXT_PRODUCT_ADDING = {
+  name: "",
+  barcode: "",
+  qty: 1,
+  tax,
+  price: 0,
+  updateWithNewProduct: function({name, price, tax, barcode}) {
+    this.name = name;
+    this.barcode = barcode;
+    this.price = price;
+    this.tax = tax
+  },
+  setProductQty: function(qty) {
+    this.qty += qty;
+  },
+  retrieveProduct: function() {
+    return (
+      {
+        name: this.name,
+        barcode: this.barcode,
+        qty: this.qty,
+        tax: this.tax,
+        price: this.price
+      }
+    )
+  }
+}
+
+const POS_PRODUCTS_CURRENT_CUSTOMER = {
+  products: [],
+  totalPrice: 0,
+  totalTax: 0,
+  discounts: 0,
+  paymentMethod: "",
+  refund: false,
+  refundAmount: 0,
+  cashback: 0,
+  lotteryPayout: 0,
+  changeDue: 0,
+  addNewProduct: function(product) {
+    this.totalTax += Math.round(product.price * (product.tax / 100));
+    this.totalPrice += product.price;
+    this.products.push(product);
+
+    return
+  },
+  addDiscount: function(amount) {
+    return this.discounts += amount;
   }
 }
