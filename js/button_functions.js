fce7f4a37ff4df2d47016add2c406757c3311315
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 00:59:04 +0000

    modify: added toggle popup function for find items, handle input type change

--- js/button_functions.js
+++ js/button_functions.js
@@ -16,6 +16,29 @@ function toggleCloseCashDrawPopup() {
   CLOSE_CASH_DRAW_POPUP.classList.remove("hidden");
 }
 
+function toggleFindItemsPopup() {
+  const allChildrenButtons = Object.values(document.querySelector(".main_pos_body").getElementsByTagName("button"));
+
+  if (POS_SYSTEM_DEFAULT.FIND_ITEMS_STATUS) {
+    POS_SYSTEM_DEFAULT.FIND_ITEMS_TOGGLE();
+
+    allChildrenButtons.forEach(button => button.disabled = false);
+    MAIN_POS_BODY.style.filter = "none";
+
+    return FIND_ITEMS_POPUP.classList.add("hidden");
+  }
+
+  allChildrenButtons.forEach(button => button.disabled = true);
+  MAIN_POS_BODY.style.filter = "Blur(4px)";
+  POS_SYSTEM_DEFAULT.FIND_ITEMS_TOGGLE();
+  FIND_ITEMS_POPUP.classList.remove("hidden");
+}
+
 const handleKeyPadInput = value => KEYPAD_INPUT_DISPLAY.value += value;
 const handleKeyPadClear = value => KEYPAD_INPUT_DISPLAY.value = value;
 const handleKeyPadClearFull = () => KEYPAD_INPUT_DISPLAY.value = "";
+
+const handleKeyPadInputTypeChange = newType => {
+  POS_SYSTEM_DEFAULT.CHANGE_KEYPAD_INPUT_TYPE(newType);
+  KEYPAD_INPUT_DISPLAY.placeholder = newType;
+}
