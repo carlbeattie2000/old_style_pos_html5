3f6421838b04cd3e817ab74926d846f4daf6aff2
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 01:01:36 +0000

    modify: added find items popup var

--- js/ui_elements.js
+++ js/ui_elements.js
@@ -1,4 +1,6 @@
 const ITEMS_IN_BASKET_DISPLAY = document.getElementById("items_basket");
 const CLOSE_CASH_DRAW_POPUP = document.getElementById("close_cash_draw");
+const FIND_ITEMS_POPUP = document.getElementById("find_items_popup");
+const FIND_ITEMS_ITEM_DISPLAY = document.getElementById("item_lookup_display_items");
 const MAIN_POS_BODY = document.querySelector(".main_pos_body");
 const KEYPAD_INPUT_DISPLAY = document.getElementById("keypad_input_display")
