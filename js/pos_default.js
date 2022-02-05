51d59ca7153c5c02c1aa832220a76a9aff930628
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 01:01:08 +0000

    modify: keyboard input type, product list

--- js/pos_default.js
+++ js/pos_default.js
@@ -1,9 +1,21 @@
 const POS_SYSTEM_DEFAULT = {
+  KEYPAD_INPUT_TYPE: "barcode",
+  CHANGE_KEYPAD_INPUT_TYPE: function(type) {
+    this.KEYPAD_INPUT_TYPE = type
+  },
   CASH_DRAW_STATUS: false,
   OPEN_CASH_DRAW: function() {
     return this.CASH_DRAW_STATUS = true
   },
   CLOSE_CASH_DRAW: function() {
     return this.CASH_DRAW_STATUS = false
+  },
+  FIND_ITEMS_STATUS: false,
+  FIND_ITEMS_TOGGLE: function() {
+    this.FIND_ITEMS_STATUS = !this.FIND_ITEMS_STATUS
+  },
+  ITEMS_LIST: [],
+  LOAD_ITEMS_LIST: function(list) {
+    this.ITEMS_LIST = list;
   }
 }
