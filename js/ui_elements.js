225d98fdc32d3bd73cf58fc08fd21c35cfb99fc3
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-05 15:19:58 +0000

    removed: duplicate

--- js/ui_elements.js
+++ js/ui_elements.js
@@ -1,7 +1,11 @@
-const ITEMS_IN_BASKET_DISPLAY = document.getElementById("items_basket");
 const CLOSE_CASH_DRAW_POPUP = document.getElementById("close_cash_draw");
 const FIND_ITEMS_POPUP = document.getElementById("find_items_popup");
 const FIND_ITEMS_ITEM_DISPLAY = document.getElementById("item_lookup_display_items");
 const MAIN_POS_BODY = document.querySelector(".main_pos_body");
 const KEYPAD_INPUT_DISPLAY = document.getElementById("keypad_input_display");
 const CUSTOMER_ITEMS_DISPLAY = document.getElementById("items_basket");
+const CUSTOMER_ITEMS_DISPLAY_TOTAL = document.getElementById("item_display_total");
+
+// reset values, will be moved to POS_DEFAULT
+CUSTOMER_ITEMS_DISPLAY_TOTAL.value = "";
+KEYPAD_INPUT_DISPLAY.value = "";
