1e0e57b6329f1b2089684dece4f50051dea8d02a
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-06 22:54:33 +0000

    added keypad input event

--- js/buttons.js
+++ js/buttons.js
@@ -3,6 +3,7 @@ import UI_FUNCTIONS from './ui_functions.js';
 const POS_BUTTONS = {
   openCashRegister: document.getElementById('cash_register_open'),
   closeCashRegister: document.getElementById('close_cash_draw_button'),
+  keypadButtons: document.querySelectorAll('.keypad_button'),
 };
 
 POS_BUTTONS.openCashRegister.addEventListener('click', () => {
@@ -11,4 +12,12 @@ POS_BUTTONS.openCashRegister.addEventListener('click', () => {
 
 POS_BUTTONS.closeCashRegister.addEventListener('click', () => {
   UI_FUNCTIONS.closeCashDrawOpenPopup();
+});
+
+POS_BUTTONS.keypadButtons.forEach((keypadButton) => {
+  const keypadButtonSelected = keypadButton;
+
+  keypadButtonSelected.addEventListener('click', (e) => {
+    UI_FUNCTIONS.addCharacterToInputValue(e.target.value);
+  });
 });
