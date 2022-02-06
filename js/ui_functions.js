6c8d3db41c4365d91ac699b9f79fdcc2afb0aa1d
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-06 02:17:43 +0000

    modify: checking if item already inside cart function, and tax custom row

--- js/ui_functions.js
+++ js/ui_functions.js
@@ -6,30 +6,32 @@ function addProductTableRow(product) {
       <td>${product.barcode}</td>
       <td>${product.name}</td>
       <td id="qty_${product.barcode}">1</td>
-      <td>@${product.price}</td>
-      <td id="total_${product.barcode}">${product.price}</td>
+      <td>@${handleCurrencyFormat(product.price)}</td>
+      <td id="total_${product.barcode}">${handleCurrencyFormat(product.price)}</td>
     </tr>
   `
   )
 
 }
 
-function itemAlreadyInTable(barcode, price) {
+function itemAlreadyInTable(product) {
   let found = false;
 
   Object.values(CUSTOMER_ITEMS_DISPLAY.children).forEach(row => {
 
-     if (row.childNodes[0].id == barcode) {
-       const qty = document.getElementById(`qty_${barcode}`);
-       const total = document.getElementById(`total_${barcode}`);
+     if (row.childNodes[0].id == product.barcode) {
+      POS_PRODUCTS_CURRENT_CUSTOMER.addNewProduct(product);
 
-       qty.textContent = parseInt(qty.textContent) + 1;
+      const qty = document.getElementById(`qty_${product.barcode}`);
+      const total = document.getElementById(`total_${product.barcode}`);
 
-       total.textContent = parseInt(qty.textContent) * price;
+      qty.textContent = parseInt(qty.textContent) + 1;
 
-       found = true;
+      total.textContent = handleCurrencyFormat(parseInt(qty.textContent) * product.price);
 
-       calcTotal();
+      found = true;
+
+      calcTotal();
      }
 
   })
@@ -40,20 +42,30 @@ function itemAlreadyInTable(barcode, price) {
 function calcTotal() {
   let total = 0;
 
-  Object.values(CUSTOMER_ITEMS_DISPLAY.children).forEach(row => {
-
-    row.childNodes[0].childNodes.forEach(row_item => {
-
-      if (row_item.id) {
-        row_item.id.includes("total") ? total += parseFloat(row_item.textContent) : null 
-      }
-
-    })
-
+  POS_PRODUCTS_CURRENT_CUSTOMER.products.forEach(product => {
+    total += product.price * product.qty;
   })
 
-  return CUSTOMER_ITEMS_DISPLAY_TOTAL.value = "$" + total
+  return CUSTOMER_ITEMS_DISPLAY_TOTAL.value = "$" + handleCurrencyFormat(total);
   
+}
+
+function updateTotalDisplay(amount) {
+  CUSTOMER_ITEMS_DISPLAY_TOTAL.value = "$" + amount;
+}
+
+function customTaxTableRow (taxAmount) {
+  return (
+    `
+    <tr id="tax">
+      <td></td>
+      <td>Tax</td>
+      <td>1</td>
+      <td></td>
+      <td>${taxAmount}</td>
+    </tr>
+  `
+  )
 }
 
 function itemDisplayRemoveItems() {
