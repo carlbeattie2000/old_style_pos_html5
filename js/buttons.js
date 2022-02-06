15bb13e7122b55181cbe7f4c966320ca71371b5d
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-06 22:34:14 +0000

    fix: bug where more than one event listener was being added to a button

--- js/buttons.js
+++ js/buttons.js
@@ -2,8 +2,13 @@ import UI_FUNCTIONS from './ui_functions.js';
 
 const POS_BUTTONS = {
   openCashRegister: document.getElementById('cash_register_open'),
+  closeCashRegister: document.getElementById('close_cash_draw_button'),
 };
 
 POS_BUTTONS.openCashRegister.addEventListener('click', () => {
   UI_FUNCTIONS.openCashDrawOpenPopup();
+});
+
+POS_BUTTONS.closeCashRegister.addEventListener('click', () => {
+  UI_FUNCTIONS.closeCashDrawOpenPopup();
 });
