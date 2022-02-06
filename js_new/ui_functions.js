47cbbb39cee45701ca44102a908c9c56a3864749
carlbeattie2000 <12beattiecastp@gmail.com>
2022-02-06 19:44:30 +0000

    add: all ui functions home

---
+++ js_new/ui_functions.js
@@ -0,0 +1,23 @@
+const UI_FUNCTIONS = {
+  // ui elements attachments go here
+  warningPopupWindow(message) {
+    console.error(new Error(message)); // will show popup window with error message
+  },
+  closeWarningPopupWindow() {
+    // code to close warning popup window
+  },
+  openFindItemPopup() {
+    // run code to open find item popup
+  },
+  closeFindItemPopup() {
+    // run code to close find item popup
+  },
+  openCashDrawOpenPopup() {
+    // run code to open close cash draw popup
+  },
+  closeCashDrawOpenPopup() {
+    // run code to close cash draw open popup
+  }
+};
+
+export default UI_FUNCTIONS;
